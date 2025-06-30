dfa3fd5d6121f3ce74e095a0adfb74a871808926
mantrivinnie <mantrivinnie@gmail.com>
2025-06-30 12:37:01 +0530

    Added folder

---
+++ DSA/01_Array1/01_SearchingInUnsortedArray/SearchingInUnsortedArray.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+//In unsorted Array we can use Linear Search to find the element in the array
+int search (int arr[],int n,int x){
+    //traverse the array from start to end
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            //if element present in arr return index of the element
+            return i;
+        }
+    }
+
+    //if element is not present in the array return -1
+    return -1;
+}
+
+//Time Complexity: O(n)
+//Worst Case when element is not presnt in the array
