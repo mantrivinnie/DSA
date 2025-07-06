54464e72e90d7e54d1c8b6d2347f205e7539ef62
mantrivinnie <mantrivinnie@gmail.com>
2025-07-06 19:32:25 +0530

    Add file using naive solution

---
+++ DSA/01_Array2/14_LeftRotateByD/NaiveSolution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//We assume d << no.of elements
+
+int leftRotateOne(int arr[],int n){
+    int temp = arr[0];
+
+    for(int i=1;i<n;i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+}
+
+int leftRotate(int arr[],int n,int d){
+    for(int i=0;i<d;i++)
+        leftRotateOne(arr,n);
+}
+
+int main(){
+    int arr1[6] = {10,20,30,40,50,60};
+    int n = 6;
+    
+    cout<<"Result after left rotate by d : ";
+    leftRotate(arr1,n,2);
+    for(int i=0;i<n;i++){
+        cout<<arr1[i]<<" ";
+    }
+
+    return 0;
+}
